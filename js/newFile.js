fab35ae0d37cf6da6f3f7d7257601ca0d432da48
Anondo Ray <anondo554@gmail.com>
2024-09-17 18:57:11 +0600

    first commit

---
+++ js/newFile.js
@@ -0,0 +1,9 @@
+$('.sliderPart').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: false,
+  prevArrow: '<i class="fa-solid fa-angle-left"></i>',
+  nextArrow: '<i class="fa-solid fa-angle-right"></i>'
+});
